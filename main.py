43d29f2feef083954affefcc99eafc5524aefc43
ColoZhao <zhaoguochang4@gmail.com>
2024-06-20 11:40:00 +0800

    init

--- main.py
+++ main.py
@@ -6,4 +6,7 @@ load_dotenv('token.env')
 weather_token = os.getenv("WEATHER_TOKEN_KEY")
 wxpusher_token = os.getenv("WEATHER_PUSHER_TOKEN")
 
+if weather_token == "d1a6c7c4cac3472eaeda5710efc80ca2":
+    print("YES")
+
 print(weather_token, wxpusher_token)
